be1306c7f6a707c46e4e61768275c173daea63ff
dbbnicole <dbbnicole>
2023-07-24 19:44:30 +0000

    Migrate from init script in DBFS to WSFS

--- RUNME.py
+++ RUNME.py
@@ -1,34 +1,36 @@
 # Databricks notebook source
 # MAGIC %md This notebook sets up the companion cluster(s) to run the solution accelerator. It also creates the Workflow to illustrate the order of execution. Happy exploring! 
 # MAGIC 🎉
-# MAGIC 
+# MAGIC
 # MAGIC **Steps**
-# MAGIC 1. Simply attach this notebook to a cluster with DBR 11.0 and above, and hit Run-All for this notebook. A multi-step job and the clusters used in the job will be created for you and hyperlinks are printed on the last block of the notebook. 
-# MAGIC 
+# MAGIC 1. Simply attach this notebook to a cluster and hit Run-All for this notebook. A multi-step job and the clusters used in the job will be created for you and hyperlinks are printed on the last block of the notebook. 
+# MAGIC
 # MAGIC 2. Run the accelerator notebooks: Feel free to explore the multi-step job page and **run the Workflow**, or **run the notebooks interactively** with the cluster to see how this solution accelerator executes. 
-# MAGIC 
+# MAGIC
 # MAGIC     2a. **Run the Workflow**: Navigate to the Workflow link and hit the `Run Now` 💥. 
 # MAGIC   
 # MAGIC     2b. **Run the notebooks interactively**: Attach the notebook with the cluster(s) created and execute as described in the `job_json['tasks']` below.
-# MAGIC 
+# MAGIC
 # MAGIC **Prerequisites** 
 # MAGIC 1. You need to have cluster creation permissions in this workspace.
-# MAGIC 
+# MAGIC
 # MAGIC 2. In case the environment has cluster-policies that interfere with automated deployment, you may need to manually create the cluster in accordance with the workspace cluster policy. The `job_json` definition below still provides valuable information about the configuration these series of notebooks should run with. 
-# MAGIC 
+# MAGIC
 # MAGIC **Notes**
 # MAGIC 1. The pipelines, workflows and clusters created in this script are not user-specific. Keep in mind that rerunning this script again after modification resets them for other users too.
-# MAGIC 
+# MAGIC
 # MAGIC 2. If the job execution fails, please confirm that you have set up other environment dependencies as specified in the accelerator notebooks. Accelerators may require the user to set up additional cloud infra or secrets to manage credentials. 
 
 # COMMAND ----------
 
 # DBTITLE 0,Install util packages
 # MAGIC %pip install git+https://github.com/databricks-academy/dbacademy@v1.0.13 git+https://github.com/databricks-industry-solutions/notebook-solution-companion@safe-print-html --quiet --disable-pip-version-check
+# MAGIC dbutils.library.restartPython()
 
 # COMMAND ----------
 
 from solacc.companion import NotebookSolutionCompanion
+nsc = NotebookSolutionCompanion()
 
 # COMMAND ----------
 
@@ -92,8 +94,8 @@ job_json = {
                     "node_type_id": {"AWS": "i3.4xlarge", "MSA": "Standard_E16_v3", "GCP": "n1-highmem-16"},
                     "init_scripts": [
                         {
-                            "dbfs": {
-                                "destination": "dbfs:/databricks/scripts/osrm-backend.sh"
+                            "workspace": {
+                                "destination": f"{nsc.solacc_path}/osrm-backend.sh"
                             }
                         }
                     ]
@@ -106,7 +108,7 @@ job_json = {
 
 dbutils.widgets.dropdown("run_job", "False", ["True", "False"])
 run_job = dbutils.widgets.get("run_job") == "True"
-NotebookSolutionCompanion().deploy_compute(job_json, run_job=run_job)
+nsc.deploy_compute(job_json, run_job=run_job)
 
 # COMMAND ----------
 
---
+++ osrm-backend.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [[ $DB_IS_DRIVER != "TRUE" ]]; then  
+
+  echo "installing osrm backend server dependencies"
+  sudo apt -qq install -y build-essential git cmake pkg-config libbz2-dev libxml2-dev libzip-dev libboost-all-dev lua5.2 liblua5.2-dev libtbb-dev
+  
+  echo "launching osrm backend server"
+  /dbfs/FileStore/osrm-backend/build/osrm-routed --algorithm=MLD /dbfs/FileStore/osrm-backend/maps/north-america/north-america-latest.osrm &
+  
+  echo "wait until osrm backend server becomes responsive"
+  res=-1
+  i=1
+
+  # while no response
+  while [ $res -ne 0 ]
+  do
+
+    # test connectivity
+    curl --silent "http://127.0.0.1:5000/route/v1/driving/-74.005310,40.708750;-73.978691,40.744850"
+    res=$?
+    
+    # increment the loop counter
+    if [ $i -gt 40 ] 
+    then 
+      break
+    fi
+    i=$(( $i + 1 ))
+
+    # if no response, sleep
+    if [ $res -ne 0 ]
+    then
+      sleep 30
+    fi
+
+  done  
+  
+fi
